92bcb2d127d6485653f240e82c1d0a9eebb4d773
smshields <samuel.m.shields@gmail.com>
2021-11-28 16:33:37 -0800

    semi-correct renaming of torus variable names
    added some better bounds and made it accurate to the torus. got halfway through before I realized github editor is the worst place to do this

--- orbit.js
+++ orbit.js
@@ -52,165 +52,160 @@ let cameraAngle = 0;
 function setup() {
     getAudioContext().suspend();
     //Takes up browser screen
-    //TODO: Remove scroll bars
     let cnv = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
-    //Move around the world with drag
-
-    //Establish 15 voices for all available keys
-
-    //Generate Shapes for Visualizer
-
+    //variable to measure output of sound
     amplitude = new p5.Amplitude();
-
+    //TODO: Reduce this mess - this is not great code, but it's what my tired brain did for a deadline, and it works.
+    //I'll optimize it or make it generic if this project continues.
     monoSynth1 = new p5.MonoSynth();
     monoSynth1.num = 0;
-    monoSynth1.sphereX = randomX();
-    monoSynth1.sphereY = randomY();
-    monoSynth1.sphereZ = randomZ();
+    monoSynth1.sphereX = randomTorusRadius();
+    monoSynth1.sphereY = randomTorusTubeRadius();
+    monoSynth1.sphereZ = randomTorusDetailX();
     monoSynth1.rotX = randomRotateFactor();
     monoSynth1.rotY = randomRotateFactor();
     monoSynth1.rotZ = randomRotateFactor();
-    monoSynth1.sphereSize = randomSize();
+    monoSynth1.sphereSize = randomTorusDetailY();
     monoSynth1.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth2 = new p5.MonoSynth();
     monoSynth2.num = 1;
-    monoSynth2.sphereX = randomX();
-    monoSynth2.sphereY = randomY();
-    monoSynth2.sphereZ = randomZ();
+    monoSynth2.sphereX = randomTorusRadius();
+    monoSynth2.sphereY = randomTorusTubeRadius();
+    monoSynth2.sphereZ = randomTorusDetailX()
     monoSynth2.rotX = randomRotateFactor();
     monoSynth2.rotY = randomRotateFactor();
     monoSynth2.rotZ = randomRotateFactor();
-    monoSynth2.sphereSize = randomSize();
+    monoSynth2.sphereSize = randomTorusDetailY();
     monoSynth2.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth3 = new p5.MonoSynth();
     monoSynth3.num = 2;
-    monoSynth3.sphereX = randomX();
-    monoSynth3.sphereY = randomY();
-    monoSynth3.sphereZ = randomZ();
+    monoSynth3.sphereX = randomTorusRadius();
+    monoSynth3.sphereY = randomTorusTubeRadius();
+    monoSynth3.sphereZ = randomTorusDetailX()
     monoSynth3.rotX = randomRotateFactor();
     monoSynth3.rotY = randomRotateFactor();
     monoSynth3.rotZ = randomRotateFactor();
-    monoSynth3.sphereSize = randomSize();
+    monoSynth3.sphereSize = randomTorusDetailY();
     monoSynth3.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth4 = new p5.MonoSynth();
     monoSynth4.num = 3;
-    monoSynth4.sphereX = randomX();
-    monoSynth4.sphereY = randomY();
-    monoSynth4.sphereZ = randomZ();
+    monoSynth4.sphereX = randomTorusRadius();
+    monoSynth4.sphereY = randomTorusTubeRadius();
+    monoSynth4.sphereZ = randomTorusDetailX();
     monoSynth4.rotX = randomRotateFactor();
     monoSynth4.rotY = randomRotateFactor();
     monoSynth4.rotZ = randomRotateFactor();
-    monoSynth4.sphereSize = randomSize();
+    monoSynth4.sphereSize = randomTorusDetailY();
     monoSynth4.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth5 = new p5.MonoSynth();
     monoSynth5.num = 4;
-    monoSynth5.sphereX = randomX();
-    monoSynth5.sphereY = randomY();
-    monoSynth5.sphereZ = randomZ();
+    monoSynth5.sphereX = randomTorusRadius();
+    monoSynth5.sphereY = randomTorusTubeRadius();
+    monoSynth5.sphereZ = randomTorusDetailX();
     monoSynth5.rotX = randomRotateFactor();
     monoSynth5.rotY = randomRotateFactor();
     monoSynth5.rotZ = randomRotateFactor();
-    monoSynth5.sphereSize = randomSize();
+    monoSynth5.sphereSize = randomTorusDetailY();
     monoSynth5.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth6 = new p5.MonoSynth();
     monoSynth6.num = 5;
-    monoSynth6.sphereX = randomX();
-    monoSynth6.sphereY = randomY();
-    monoSynth6.sphereZ = randomZ();
+    monoSynth6.sphereX = randomTorusRadius();
+    monoSynth6.sphereY = randomTorusTubeRadius();
+    monoSynth6.sphereZ = randomTorusDetailX();
     monoSynth6.rotX = randomRotateFactor();
     monoSynth6.rotY = randomRotateFactor();
     monoSynth6.rotZ = randomRotateFactor();
-    monoSynth6.sphereSize = randomSize();
+    monoSynth6.sphereSize = randomTorusDetailY();
     monoSynth6.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth7 = new p5.MonoSynth();
     monoSynth7.num = 6;
-    monoSynth7.sphereX = randomX();
-    monoSynth7.sphereY = randomY();
-    monoSynth7.sphereZ = randomZ();
+    monoSynth7.sphereX = randomTorusRadius();
+    monoSynth7.sphereY = randomTorusTubeRadius();
+    monoSynth7.sphereZ = randomTorusDetailX();
     monoSynth7.rotX = randomRotateFactor();
     monoSynth7.rotY = randomRotateFactor();
     monoSynth7.rotZ = randomRotateFactor();
-    monoSynth7.sphereSize = randomSize();
+    monoSynth7.sphereSize = randomTorusDetailY();
     monoSynth7.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth8 = new p5.MonoSynth();
     monoSynth8.num = 7;
-    monoSynth8.sphereX = randomX();
-    monoSynth8.sphereY = randomY();
-    monoSynth8.sphereZ = randomZ();
+    monoSynth8.sphereX = randomTorusRadius();
+    monoSynth8.sphereY = randomTorusTubeRadius();
+    monoSynth8.sphereZ = randomTorusDetailX();
     monoSynth8.rotX = randomRotateFactor();
     monoSynth8.rotY = randomRotateFactor();
     monoSynth8.rotZ = randomRotateFactor();
-    monoSynth8.sphereSize = randomSize();
+    monoSynth8.sphereSize = randomTorusDetailY();
     monoSynth8.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth9 = new p5.MonoSynth();
     monoSynth9.num = 8;
-    monoSynth9.sphereX = randomX();
-    monoSynth9.sphereY = randomY();
-    monoSynth9.sphereZ = randomZ();
+    monoSynth9.sphereX = randomTorusRadius();
+    monoSynth9.sphereY = randomTorusTubeRadius();
+    monoSynth9.sphereZ = randomTorusDetailX();
     monoSynth9.rotX = randomRotateFactor();
     monoSynth9.rotY = randomRotateFactor();
     monoSynth9.rotZ = randomRotateFactor();
-    monoSynth9.sphereSize = randomSize();
+    monoSynth9.sphereSize = randomTorusDetailY();
     monoSynth9.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth10 = new p5.MonoSynth();
     monoSynth10.num = 9;
-    monoSynth10.sphereX = randomX();
-    monoSynth10.sphereY = randomY();
-    monoSynth10.sphereZ = randomZ();
+    monoSynth10.sphereX = randomTorusRadius();
+    monoSynth10.sphereY = randomTorusTubeRadius();
+    monoSynth10.sphereZ = randomTorusDetailX();
     monoSynth10.rotX = randomRotateFactor();
     monoSynth10.rotY = randomRotateFactor();
     monoSynth10.rotZ = randomRotateFactor();
-    monoSynth10.sphereSize = randomSize();
+    monoSynth10.sphereSize = randomTorusDetailY();
     monoSynth10.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth11 = new p5.MonoSynth();
     monoSynth11.num = 10;
-    monoSynth11.sphereX = randomX();
-    monoSynth11.sphereY = randomY();
-    monoSynth11.sphereZ = randomZ();
+    monoSynth11.sphereX = randomTorusRadius();
+    monoSynth11.sphereY = randomTorusTubeRadius();
+    monoSynth11.sphereZ = randomTorusDetailX();
     monoSynth11.rotX = randomRotateFactor();
     monoSynth11.rotY = randomRotateFactor();
     monoSynth11.rotZ = randomRotateFactor();
-    monoSynth11.sphereSize = randomSize();
+    monoSynth11.sphereSize = randomTorusDetailY();
     monoSynth11.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth12 = new p5.MonoSynth();
     monoSynth12.num = 11;
-    monoSynth12.sphereX = randomX();
-    monoSynth12.sphereY = randomY();
-    monoSynth12.sphereZ = randomZ();
+    monoSynth12.sphereX = randomTorusRadius();
+    monoSynth12.sphereY = randomTorusTubeRadius();
+    monoSynth12.sphereZ = randomTorusDetailX()
     monoSynth12.rotX = randomRotateFactor();
     monoSynth12.rotY = randomRotateFactor();
     monoSynth12.rotZ = randomRotateFactor();
-    monoSynth12.sphereSize = randomSize();
+    monoSynth12.sphereSize = randomTorusDetailY();
     monoSynth12.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth13 = new p5.MonoSynth();
     monoSynth13.num = 12;
-    monoSynth13.sphereX = randomX();
-    monoSynth13.sphereY = randomY();
-    monoSynth13.sphereZ = randomZ();
+    monoSynth13.sphereX = randomTorusRadius();
+    monoSynth13.sphereY = randomTorusTubeRadius();
+    monoSynth13.sphereZ = randomTorusDetailX();
     monoSynth13.rotX = randomRotateFactor();
     monoSynth13.rotY = randomRotateFactor();
     monoSynth13.rotZ = randomRotateFactor();
-    monoSynth13.sphereSize = randomSize();
+    monoSynth13.sphereSize = randomTorusDetailY();
     monoSynth13.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth14 = new p5.MonoSynth();
     monoSynth14.num = 13;
-    monoSynth14.sphereX = randomX();
-    monoSynth14.sphereY = randomY();
-    monoSynth14.sphereZ = randomZ();
+    monoSynth14.sphereX = randomTorusRadius();
+    monoSynth14.sphereY = randomTorusTubeRadius();
+    monoSynth14.sphereZ = randomTorusDetailX();
     monoSynth14.rotX = randomRotateFactor();
     monoSynth14.rotY = randomRotateFactor();
     monoSynth14.rotZ = randomRotateFactor();
-    monoSynth14.sphereSize = randomSize();
+    monoSynth14.sphereSize = randomTorusDetailY();
     monoSynth14.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
     monoSynth15 = new p5.MonoSynth();
     monoSynth15.num = 14;
-    monoSynth15.sphereX = randomX();
-    monoSynth15.sphereY = randomY();
-    monoSynth15.sphereZ = randomZ();
+    monoSynth15.sphereX = randomTorusRadius();
+    monoSynth15.sphereY = randomTorusTubeRadius();
+    monoSynth15.sphereZ = randomTorusDetailX();
     monoSynth15.rotX = randomRotateFactor();
     monoSynth15.rotY = randomRotateFactor();
     monoSynth15.rotZ = randomRotateFactor();
-    monoSynth15.sphereSize = randomSize();
+    monoSynth15.sphereSize = randomTorusDetailY();
     monoSynth15.fillColor = color(random(0, 250), random(0, 250), random(0, 250));
 }
 
@@ -289,18 +284,24 @@ function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight, WEBGL);
 }
 
-function randomX() {
+/** Torus definition and parameters pulled from P5 docs
+*/
+function randomTorusRadius() {
+    return Math.round(random(400)) ;
+}
+
+function randomTorusTubeRadius() {
     return Math.round(random(300));
 }
-function randomY() {
-    return Math.round(random(300));
+
+function randomTorusDetailX() {
+    return Math.round(random(24));
 }
-function randomZ() {
-    return Math.round(random(300));
+
+function randomTorusDetailY() {
+    return Math.round(random(16));
 }
-function randomSize() {
-    return Math.round(random(10, 300));
-}
+
 function randomRotateFactor() {
     return random() * .01;
 }
@@ -326,6 +327,7 @@ function triggerMonoSynthRelease(monoSynth) {
 }
 
 //provide 1, 3, 5, 7, 9 in multiple octaves... maybe a blues?
+//creates the sound and visual for the user on key depression
 function keyPressed() {
 
     //Row 1
@@ -382,6 +384,7 @@ function keyPressed() {
     return false;
 }
 
+//Turns the sound and visual off when the user releases a given key. 
 function keyReleased(event) {
 
     //Row 1
@@ -434,22 +437,6 @@ function keyReleased(event) {
     if (key === 'b') {
         triggerMonoSynthRelease(monoSynth15);
     }
-
     return false;
-}
-
-//Render, in a location, a shape of some kind
-function renderSphere(monoSynth) {
-    noStroke();
-    console.log(monoSynth.fillColor);
-    ambientMaterial(monoSynth.fillColor);
-    push();
-        translate(monoSynth.sphereX, monoSynth.sphereY, monoSynth.sphereZ);
-        rotateZ(frameCount * monoSynth.rotZ);
-        rotateY(frameCount * monoSynth.rotY);
-        rotateX(frameCount * monoSynth.rotX);
-    rotateY(frameCount * 0.01);
-    torus(monoSynth.sphereX, monoSynth.sphereY, monoSynth.sphereZ, monoSynth.sphereSize);
-    pop();
 }
 
